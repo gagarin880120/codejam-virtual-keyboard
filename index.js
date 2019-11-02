c303c13343b2849102e9d481b47102bb8c4c534b
Andrei M <gagarin880120@gmail.com>
2019-11-03 02:44:40 +0300

    feat: add animation, capslock behavior and laguage switcher (alt+shift)

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ row1.className = 'row';
 const row1Array = [];
 
 const backquoteBt = document.createElement('div');
-backquoteBt.className = 'number';
-backquoteBt.innerText = '`';
+backquoteBt.className = 'letter';
+backquoteBt.eng = '`';
+backquoteBt.cyr = 'ё';
 backquoteBt.code = 192;
 
 row1Array.push(backquoteBt);
@@ -127,84 +128,96 @@ row2Array.push(tabBtn);
 
 const letterQBtn = document.createElement('div');
 letterQBtn.className = 'letter';
-letterQBtn.innerText = 'q';
+letterQBtn.eng = 'q';
+letterQBtn.cyr = 'й';
 letterQBtn.code = 81;
 
 row2Array.push(letterQBtn);
 
 const letterWBtn = document.createElement('div');
 letterWBtn.className = 'letter';
-letterWBtn.innerText = 'w';
+letterWBtn.eng = 'w';
+letterWBtn.cyr = 'ц';
 letterWBtn.code = 87;
 
 row2Array.push(letterWBtn);
 
 const letterEBtn = document.createElement('div');
 letterEBtn.className = 'letter';
-letterEBtn.innerText = 'e';
+letterEBtn.eng = 'e';
+letterEBtn.cyr = 'у';
 letterEBtn.code = 69;
 
 row2Array.push(letterEBtn);
 
 const letterRBtn = document.createElement('div');
 letterRBtn.className = 'letter';
-letterRBtn.innerText = 'r';
+letterRBtn.eng = 'r';
+letterRBtn.cyr = 'к';
 letterRBtn.code = 82;
 
 row2Array.push(letterRBtn);
 
 const letterTBtn = document.createElement('div');
 letterTBtn.className = 'letter';
-letterTBtn.innerText = 't';
+letterTBtn.eng = 't';
+letterTBtn.cyr = 'е';
 letterTBtn.code = 84;
 
 row2Array.push(letterTBtn);
 
 const letterYBtn = document.createElement('div');
 letterYBtn.className = 'letter';
-letterYBtn.innerText = 'y';
+letterYBtn.eng = 'y';
+letterYBtn.cyr = 'н';
 letterYBtn.code = 89;
 
 row2Array.push(letterYBtn);
 
 const letterUBtn = document.createElement('div');
 letterUBtn.className = 'letter';
-letterUBtn.innerText = 'u';
+letterUBtn.eng = 'u';
+letterUBtn.cyr = 'г';
 letterUBtn.code = 85;
 
 row2Array.push(letterUBtn);
 
 const letterIBtn = document.createElement('div');
 letterIBtn.className = 'letter';
-letterIBtn.innerText = 'i';
+letterIBtn.eng = 'i';
+letterIBtn.cyr = 'ш';
 letterIBtn.code = 73;
 
 row2Array.push(letterIBtn);
 
 const letterOBtn = document.createElement('div');
 letterOBtn.className = 'letter';
-letterOBtn.innerText = 'o';
+letterOBtn.eng = 'o';
+letterOBtn.cyr = 'щ';
 letterOBtn.code = 79;
 
 row2Array.push(letterOBtn);
 
 const letterPBtn = document.createElement('div');
 letterPBtn.className = 'letter';
-letterPBtn.innerText = 'p';
+letterPBtn.eng = 'p';
+letterPBtn.cyr = 'з';
 letterPBtn.code = 80;
 
 row2Array.push(letterPBtn);
 
 const letterBracketLeftBtn = document.createElement('div');
 letterBracketLeftBtn.className = 'letter';
-letterBracketLeftBtn.innerText = '[';
+letterBracketLeftBtn.eng = '[';
+letterBracketLeftBtn.cyr = 'х';
 letterBracketLeftBtn.code = 219;
 
 row2Array.push(letterBracketLeftBtn);
 
 const letterBracketRightBtn = document.createElement('div');
 letterBracketRightBtn.className = 'letter';
-letterBracketRightBtn.innerText = ']';
+letterBracketRightBtn.eng = ']';
+letterBracketRightBtn.cyr = 'ъ';
 letterBracketRightBtn.code = 221;
 
 row2Array.push(letterBracketLeftBtn);
@@ -235,77 +248,88 @@ row3Array.push(capsLockBtn);
 
 const letterABtn = document.createElement('div');
 letterABtn.className = 'letter';
-letterABtn.innerText = 'a';
+letterABtn.eng = 'a';
+letterABtn.cyr = 'ф';
 letterABtn.code = 65;
 
 row3Array.push(letterABtn);
 
 const letterSBtn = document.createElement('div');
 letterSBtn.className = 'letter';
-letterSBtn.innerText = 's';
+letterSBtn.eng = 's';
+letterSBtn.cyr = 'ы';
 letterSBtn.code = 83;
 
 row3Array.push(letterSBtn);
 
 const letterDBtn = document.createElement('div');
 letterDBtn.className = 'letter';
-letterDBtn.innerText = 'd';
+letterDBtn.eng = 'd';
+letterDBtn.cyr = 'в';
 letterDBtn.code = 68;
 
 row3Array.push(letterDBtn);
 
 const letterFBtn = document.createElement('div');
 letterFBtn.className = 'letter';
-letterFBtn.innerText = 'f';
+letterFBtn.eng = 'f';
+letterFBtn.cyr = 'а';
 letterFBtn.code = 70;
 
 row3Array.push(letterFBtn);
 
 const letterGBtn = document.createElement('div');
 letterGBtn.className = 'letter';
-letterGBtn.innerText = 'g';
+letterGBtn.eng = 'g';
+letterGBtn.cyr = 'п';
 letterGBtn.code = 71;
 
 row3Array.push(letterGBtn);
 
 const letterHBtn = document.createElement('div');
 letterHBtn.className = 'letter';
-letterHBtn.innerText = 'h';
+letterHBtn.eng = 'h';
+letterHBtn.cyr = 'р';
 letterHBtn.code = 72;
 
 row3Array.push(letterHBtn);
 
 const letterJBtn = document.createElement('div');
 letterJBtn.className = 'letter';
-letterJBtn.innerText = 'j';
+letterJBtn.eng = 'j';
+letterJBtn.cyr = 'о';
 letterJBtn.code = 74;
 
 row3Array.push(letterJBtn);
 
 const letterKBtn = document.createElement('div');
 letterKBtn.className = 'letter';
-letterKBtn.innerText = 'k';
+letterKBtn.eng = 'k';
+letterKBtn.cyr = 'л';
 letterKBtn.code = 75;
 
 row3Array.push(letterKBtn);
 
 const letterLBtn = document.createElement('div');
 letterLBtn.className = 'letter';
-letterLBtn.innerText = 'l';
+letterLBtn.eng = 'l';
+letterLBtn.cyr = 'д';
 letterLBtn.code = 76;
 
 row3Array.push(letterLBtn);
 
 const semicolonBtn = document.createElement('div');
 semicolonBtn.className = 'letter';
-semicolonBtn.innerText = ';';
+semicolonBtn.eng = ';';
+semicolonBtn.cyr = 'ж';
 semicolonBtn.code = 186;
 
 row3Array.push(semicolonBtn);
 
 const quoteBtn = document.createElement('div');
 quoteBtn.className = 'letter';
-quoteBtn.innerText = '\'';
+quoteBtn.eng = '\'';
+quoteBtn.cyr = 'э';
 quoteBtn.code = 222;
 
 row3Array.push(quoteBtn);
@@ -336,76 +360,86 @@ row4Array.push(shiftLeftBtn);
 
 const letterZBtn = document.createElement('div');
 letterZBtn.className = 'letter';
-letterZBtn.innerText = 'z';
+letterZBtn.eng = 'z';
+letterZBtn.cyr = 'я';
 letterZBtn.code = 90;
 
 row4Array.push(letterZBtn);
 
 const letterXBtn = document.createElement('div');
 letterXBtn.className = 'letter';
-letterXBtn.innerText = 'x';
+letterXBtn.eng = 'x';
+letterXBtn.cyr = 'ч';
 letterXBtn.code = 88;
 
 row4Array.push(letterXBtn);
 
 const letterCBtn = document.createElement('div');
 letterCBtn.className = 'letter';
-letterCBtn.innerText = 'c';
+letterCBtn.eng = 'c';
+letterCBtn.cyr = 'с';
 letterCBtn.code = 67;
 
 row4Array.push(letterCBtn);
 
 const letterVBtn = document.createElement('div');
 letterVBtn.className = 'letter';
-letterVBtn.innerText = 'v';
+letterVBtn.eng = 'v';
+letterVBtn.cyr = 'м';
 letterVBtn.code = 86;
 
 row4Array.push(letterVBtn);
 
 const letterBBtn = document.createElement('div');
 letterBBtn.className = 'letter';
-letterBBtn.innerText = 'b';
+letterBBtn.eng = 'b';
+letterBBtn.cyr = 'и';
 letterBBtn.code = 66;
 
 row4Array.push(letterBBtn);
 
 const letterNBtn = document.createElement('div');
 letterNBtn.className = 'letter';
-letterNBtn.innerText = 'n';
+letterNBtn.eng = 'n';
+letterNBtn.cyr = 'т';
 letterNBtn.code = 78;
 
 row4Array.push(letterNBtn);
 
 const letterMBtn = document.createElement('div');
 letterMBtn.className = 'letter';
-letterMBtn.innerText = 'm';
+letterMBtn.eng = 'm';
+letterMBtn.cyr = 'ь';
 letterMBtn.code = 77;
 
 row4Array.push(letterMBtn);
 
 const commaBtn = document.createElement('div');
 commaBtn.className = 'letter';
-commaBtn.innerText = ',';
+commaBtn.eng = ',';
+commaBtn.cyr = 'б';
 commaBtn.code = 188;
 
 row4Array.push(commaBtn);
 
 const periodBtn = document.createElement('div');
 periodBtn.className = 'letter';
-periodBtn.innerText = '.';
+periodBtn.eng = '.';
+periodBtn.cyr = 'ю';
 periodBtn.code = 190;
 
 row4Array.push(periodBtn);
 
 const slashBtn = document.createElement('div');
 slashBtn.className = 'letter';
-slashBtn.innerText = '/';
+slashBtn.eng = '/';
+slashBtn.cyr = '.';
 slashBtn.code = 191;
 
 row4Array.push(slashBtn);
 
 const arrowUpBtn = document.createElement('div');
-arrowUpBtn.className = 'letter';
+arrowUpBtn.className = 'arrow';
 arrowUpBtn.innerText = '▲';
 arrowUpBtn.code = 38;
 
@@ -463,21 +497,21 @@ altRightBtn.code = 18;
 row5Array.push(altRightBtn);
 
 const arrowLeftBtn = document.createElement('div');
-arrowLeftBtn.className = 'letter';
+arrowLeftBtn.className = 'arrow';
 arrowLeftBtn.innerText = '◄';
 arrowLeftBtn.code = 37;
 
 row5Array.push(arrowLeftBtn);
 
 const arrowDownBtn = document.createElement('div');
-arrowDownBtn.className = 'letter';
+arrowDownBtn.className = 'arrow';
 arrowDownBtn.innerText = '▼';
 arrowDownBtn.code = 40;
 
 row5Array.push(arrowDownBtn);
 
 const arrowRightBtn = document.createElement('div');
-arrowRightBtn.className = 'letter';
+arrowRightBtn.className = 'arrow';
 arrowRightBtn.innerText = '►';
 arrowRightBtn.code = 39;
 
@@ -504,11 +538,54 @@ for (let i = 0; i < arrayOfElements.length; i += 1) {
 
 document.body.append(wrapper);
 
-const buttons = [...row1Array, ...row2Array, ...row3Array, ...row4Array, ...row5Array];
+const letters = document.getElementsByClassName('letter');
 
-console.log(buttons[1].code);
+let letterMode = 'eng';
+
+function draw() {
+  for (let i = 0; i < letters.length; i += 1) {
+    letters[i].innerText = letters[i][letterMode];
+  }
+}
+
+function setLang() {
+  if (letterMode === 'cyr') {
+    letterMode = 'eng';
+  } else {
+    letterMode = 'cyr';
+  }
+  return letterMode;
+}
 
 document.addEventListener('keydown', (event) => {
+  if (event.altKey && event.which === 16) {
+    setLang();
+    draw();
+  }
+});
+
+localStorage.setItem(letterMode, setLang());
+
+draw();
+
+document.addEventListener('keyup', (event) => {
+  if (event.which === 20) {
+    for (let i = 0; i < letters.length; i += 1) {
+      if (letters[i].innerText === letters[i].innerText.toUpperCase()) {
+        letters[i].innerText = letters[i].innerText.toLowerCase();
+      } else {
+        letters[i].innerText = letters[i].innerText.toUpperCase();
+      }
+    }
+  }
+});
+
+const buttons = [...row1Array, ...row2Array, ...row3Array, ...row4Array, ...row5Array];
+
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode === 9 || event.keyCode === 18) {
+    event.preventDefault();
+  }
 
   if (event.keyCode === 16 && event.location === 1) {
     shiftLeftBtn.classList.toggle('active');
@@ -532,17 +609,13 @@ document.addEventListener('keydown', (event) => {
   }
 
   for (let i = 0; i < buttons.length; i += 1) {
-
     if (event.keyCode === buttons[i].code && event.keyCode !== 16 && event.keyCode !== 17 && event.keyCode !== 18) {
       buttons[i].classList.toggle('active');
     }
   }
-  
 });
 
-
 document.addEventListener('keyup', (event) => {
-
   if (event.keyCode === 16 && event.location === 1) {
     shiftLeftBtn.classList.remove('active');
   }
